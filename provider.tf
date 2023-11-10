88666b2c7b4ece3e1440be45a19511e4fc71d5c5
ajsampang67 <ajsampang67@gmail.com>
2023-11-10 17:50:59 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket  = "roidtc-projects-202-tf-state"
+    prefix  = "terraform/state"
+  }
+
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
